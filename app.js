d0fcdcafea90f4182733a3ae4225632e7c3fa3fa
Mudit Meshram <144215792+Mudit48@users.noreply.github.com>
2026-02-24 09:37:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register(`${window.location.pathname}service-worker.js`)
+    navigator.serviceWorker.register(/PWAlab/sw.js)
     .then((reg) => console.log('Service Worker registered', reg))
     .catch((err) => console.log('Service Worker not registered', err));
 }
