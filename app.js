f8fe983db7c8618a22c9e83c18f091c0a652a14e
Mudit Meshram <144215792+Mudit48@users.noreply.github.com>
2026-02-24 09:40:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-if('sw' in navigator){
+if('serviceWorker' in navigator){
     navigator.serviceWorker.register(./sw.js)
     .then((reg) => console.log('Service Worker registered', reg))
     .catch((err) => console.log('Service Worker not registered', err));
